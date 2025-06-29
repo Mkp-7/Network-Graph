ea98ee475b4b08e0d82ce237da50959bc729bbad
Mkp-7 <mkpatel4102@gmail.com>
2025-06-28 23:53:00 -0400

    Update NodeGraph.py

--- NodeGraph.py
+++ NodeGraph.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.title("Interactive 3D Network Graph")
+
+# Embed the GitHub Pages-hosted HTML file
 url = "https://mkp-7.github.io/Network-Graph/a.html"
 
 components.iframe(url, height=800, scrolling=True)
